de5c4242a123c7c80c489bab319fa16d93ce4ed0
Satori32 <Huga8989+githubf@gmail.com>
2023-11-24 15:24:14 +0900

    add function.

--- ソース.cpp
+++ ソース.cpp
@@ -3,6 +3,17 @@
 
 //VC Can't compile this.
 
+template<class T,std::size_t N,std::size_t M>
+constexpr std::array<T, N> operetor = (const std::array<T, M>& In){
+	std::array<T,N> A = {0,};
+	
+	for (std::size_t i = 0; i < A.size(), i++) {
+	A[i] = In[i];
+	}
+
+	return A;
+}
+
 #define MULTIPLE_X 1.3
 template<class T, std::size_t N>
 constexpr std::array<T, std::size_t(N * MULTIPLE_X)> Expand(const std::array<T,N>& In) {
